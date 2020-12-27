6e3266efd4d5d8f6069789ecf8f7ae40d566a899
Tobias-Mauritzon <tobmau@student.chalmers.se>
2020-12-27 16:19:39 +0100

    Create Project6.ino

---
+++ Project6/Project6.ino
@@ -0,0 +1,18 @@
+
+int sensorValue;
+int sensorflow = 1023;
+int sensorHigh = 0;
+
+const int ledPin = 13;
+
+void setup()
+{
+  // put your setup code here, to run once:
+ pinMode(ledPin, OUTPUT);
+ digitalWrite(ledPin, HIGH);
+}
+
+void loop()
+{
+  
+}
