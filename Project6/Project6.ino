b6e8741953b2990a34bc3a09585dafb47e359bfe
Tobias-Mauritzon <tobmau@student.chalmers.se>
2020-12-27 16:23:45 +0100

    Update Project6.ino

--- Project6/Project6.ino
+++ Project6/Project6.ino
@@ -10,6 +10,10 @@ void setup()
   // put your setup code here, to run once:
  pinMode(ledPin, OUTPUT);
  digitalWrite(ledPin, HIGH);
+
+ while(millis() < 5000){
+     sensorValue = analogRead(A0);
+ }
 }
 
 void loop()
