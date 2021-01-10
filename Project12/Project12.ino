fe0dba0f6077f12d79e6752d11becf8580d5fba0
Tobias-Mauritzon <tobmau@student.chalmers.se>
2021-01-10 14:06:02 +0100

    Update Project12.ino

--- Project12/Project12.ino
+++ Project12/Project12.ino
@@ -1,7 +1,19 @@
-
+#include <Servo.h>
+Servo myServo;
+const int piezo = A0;
+const int switchPin = 2;
+const int yellowLed = 3;
+const int greenLed = 4;
+const int redLed = 5;
+int knockVal;
+int switchVal;
+const int quietKnock = 10;
+const int loudKnock = 100;
+boolean locked = false;
+int numberOfKnocks = 0;
 
 void setup(){
-
+    myServo.attach(9);
   
 }
 
