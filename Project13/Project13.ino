a57172084b8da5656ce8e4fab52a5fdc843813df
Tobias-Mauritzon <tobmau@student.chalmers.se>
2021-01-11 12:03:02 +0100

    Update Project13.ino

--- Project13/Project13.ino
+++ Project13/Project13.ino
@@ -1,25 +1,26 @@
 #include <CapacitiveSensor.h>
-const int piezo = A0;
-const int switchPin = 2;
-const int yellowLed = 3;
-const int greenLed = 4;
-const int redLed = 5;
+CapacitiveSensor capSensor = CapacitiveSensor(4,2);
+
+int threshold = 1000;
+const int ledPin = 12;
+
 
 
 void setup(){
-    myServo.attach(9);
-    pinMode(yellowLed,OUTPUT);
-    pinMode(redLed,OUTPUT);
-    pinMode(greenLed,OUTPUT);
-    pinMode(switchPin,INPUT);
+    pinMode(ledPin,OUTPUT);
     Serial.begin(9600);
-
-    digitalWrite(greenLed, HIGH);
-    myServo.write(0);
 }
 
 void loop(){
-  
+    long sensorValue = capSensor.capacitiveSensor(30);
+    Serial.print(sensorValue);
+
+    if(sensorValue < threshold){
+        digitalWrite(ledPin, HIGH);
+    }else{
+        digitalWrite(ledPin, LOW);
+    }
+    delay(10);
 }
 
 
