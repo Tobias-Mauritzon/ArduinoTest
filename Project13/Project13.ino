dffa6cfc23faa23bf8a0e6e2c5a3afea082b538a
Tobias-Mauritzon <tobmau@student.chalmers.se>
2021-01-11 12:24:49 +0100

    Update Project13.ino

--- Project13/Project13.ino
+++ Project13/Project13.ino
@@ -1,10 +1,10 @@
+
 #include <CapacitiveSensor.h>
+
 CapacitiveSensor capSensor = CapacitiveSensor(4,2);
 
 int threshold = 1000;
 const int ledPin = 12;
-
-
 
 void setup(){
     pinMode(ledPin,OUTPUT);
@@ -13,14 +13,12 @@ void setup(){
 
 void loop(){
     long sensorValue = capSensor.capacitiveSensor(30);
-    Serial.print(sensorValue);
+    Serial.println(sensorValue);
 
-    if(sensorValue < threshold){
+    if(sensorValue > threshold){
         digitalWrite(ledPin, HIGH);
     }else{
         digitalWrite(ledPin, LOW);
     }
-    delay(10);
+    delay(500);
 }
-
-
