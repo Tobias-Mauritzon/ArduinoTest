75f3474c4f5d2ef94a2bea3f0fa783491d4d8e81
Tobias-Mauritzon <tobmau@student.chalmers.se>
2021-02-07 13:58:47 +0100

    Update MillionLongArray.ino

--- MillionLongArray/MillionLongArray.ino
+++ MillionLongArray/MillionLongArray.ino
@@ -11,13 +11,17 @@
 */
 
 // intit array
-// 1000 long is close to max size
-char arr[1000];
+// 1000 long is close to max size for dynamic memory on the arduino
+unsigned char arr[1000];
 
 // output pins;
-int pinGreen = 1;
-int pinRed = 2;
-int pinYellow = 3;
+int pinGreen = 2;
+int pinRed = 3;
+int pinYellow = 4;
+
+int potentiometer = A0;
+int sensorValue = 0;
+int loopDelay = 10;
 
 void setup()
 {
@@ -26,13 +30,15 @@ void setup()
     // fills the array
     char c; 
     for(int i = 0; i < 1000; i++){
-        c = (rand() % 2 + 1);
+        c = (rand() % 3 + 2);
         arr[i] = c;
 
     }
 
     // init pins
-    
+    pinMode(pinGreen,OUTPUT);
+    pinMode(pinRed,OUTPUT);
+    pinMode(pinYellow,OUTPUT);
     // init console
     Serial.begin(9600);
 }
@@ -42,13 +48,38 @@ void loop()
     //Loop through the array and light a led acording to pin
     
     for(char c : arr){
+        //Read potentiometer for new daly
+        sensorValue = analogRead(potentiometer);
+        if(potentiometer > 10){
+            loopDelay = sensorValue;
+        }
+        
+        
         // Maybe i should add a delay here
+        delay(loopDelay);
+
+        //Prints loop delay in ms
+        Serial.println("Delay in ms "+ String(loopDelay));
+        
         if(c == pinGreen){
-            Serial.println("Green Pin: "+c);
+            //Ligth LEDs
+            digitalWrite(pinGreen,HIGH);
+            digitalWrite(pinRed,LOW);
+            digitalWrite(pinYellow,LOW);
+
+            Serial.println("Green Pin: "+ String(pinGreen));
         }else if(c == pinRed){
-            Serial.println("Red Pin: "+c);
+            digitalWrite(pinGreen,LOW);
+            digitalWrite(pinRed,HIGH);
+            digitalWrite(pinYellow,LOW);
+
+            Serial.println("Red Pin: "+ String(pinRed));
         }else{
-            Serial.println("Yellow Pin: "+c);
+            digitalWrite(pinGreen,LOW);
+            digitalWrite(pinRed,LOW);
+            digitalWrite(pinYellow,HIGH);
+
+            Serial.println("Yellow Pin: "+ String(pinYellow));
         }
     }
 }
