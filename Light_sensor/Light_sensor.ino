2a3f3e2c3929831ef0c83e70a15e69165f76e35c
Tobias-Mauritzon <tobmau@student.chalmers.se>
2021-05-01 15:29:50 +0200

    Create Light_sensor.ino

---
+++ Light_sensor/Light_sensor.ino
@@ -0,0 +1,15 @@
+
+// servo motor
+// 4 phototransistors for light sensing
+// led for light direction
+// move led to light source and light led
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
